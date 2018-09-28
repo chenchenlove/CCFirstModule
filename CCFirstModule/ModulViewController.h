45e17c2e529c7d598bc60791329df9cb06ac76df
smith <iOSChenC@163.com>
2018-09-28 14:32:30 +0800

    release 1.0.0

---
+++ CCFirstModule/ModulViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModulViewController.h
+//  CCModulDemo
+//
+//  Created by smith on 2018/9/27.
+//  Copyright © 2018年 陈晨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModulViewController : UIViewController
+
+@end
---
+++ CCFirstModule/ModulViewController.m
@@ -0,0 +1,38 @@
+//
+//  ModulViewController.m
+//  CCModulDemo
+//
+//  Created by smith on 2018/9/27.
+//  Copyright © 2018年 陈晨. All rights reserved.
+//
+
+#import "ModulViewController.h"
+
+@interface ModulViewController ()
+
+@end
+
+@implementation ModulViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"this is modul demo");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
